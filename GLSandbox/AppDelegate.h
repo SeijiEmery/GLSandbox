1156fdeeccaa5f76a036d40eef813e37b4bc6790
Seiji Emery <seijiemery@gmail.com>
2015-12-14 12:59:25 -0800

    Initial Commit

---
+++ GLSandbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLSandbox
+//
+//  Created by semery on 12/14/15.
+//  Copyright © 2015 Seiji Emery. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GLSandbox/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GLSandbox
+//
+//  Created by semery on 12/14/15.
+//  Copyright © 2015 Seiji Emery. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
