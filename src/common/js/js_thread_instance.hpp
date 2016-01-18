7b37dd68e611205eb82eae7fc6819b319216dde0
Seiji Emery <seijiemery@gmail.com>
2016-01-18 03:50:51 -0800

    Added debug status messages. Seems to be working correctly...

--- src/common/js/js_thread_instance.hpp
+++ src/common/js/js_thread_instance.hpp
@@ -31,7 +31,10 @@ template <typename T>
 struct ThreadedDispatchQueue {
 protected:
     typedef std::function<void(T&)> message_type;
-    typedef boost::lockfree::queue<message_type*> message_queue;
+    typedef boost::lockfree::queue<message_type*,
+        boost::lockfree::fixed_sized<true>,
+        boost::lockfree::capacity<4196>
+    > message_queue;
     
     T * m_target = nullptr;
     message_queue m_queue;
@@ -49,14 +52,18 @@ public:
     ThreadedDispatchQueue & operator= (const ThreadedDispatchQueue &) = delete;
     
     void setDispatchTarget (T * target) {
+        std::cout << "dispatch target set from thread " << std::this_thread::get_id() << '\n';
         m_target = target;
     }
     void run () {
         assert(!m_isRunning);
         m_isRunning = true;
-        m_queue.consume_all([](message_type * task) { delete task; }); // clear queue
+//        m_queue.consume_all([](message_type * task) { delete task; }); // clear queue
+        
+        std::cout << "running dispatch queue on thread " << std::this_thread::get_id() << '\n';
         do {
             while (m_isRunning && !m_queue.empty()) {
+                std::cout << "dispatch queue executing call on thread " << std::this_thread::get_id() << '\n';
                 message_type * task;
                 m_queue.pop(task);
                 (*task)(*m_target);
@@ -67,17 +74,23 @@ public:
             }
             std::unique_lock<std::mutex> lock (m_mutex);
             m_isPaused = true;
-            while (m_isRunning && m_queue.empty())
+            while (m_isRunning && m_queue.empty()) {
+                std::cout << "dispatch queue paused (" << std::this_thread::get_id() << ")\n";
                 m_cv.wait(lock);
+            }
             m_isPaused = false;
         } while (m_isRunning);
+        
+        std::cout << "dispatch queue stopped (" << std::this_thread::get_id() << ")\n";
     }
     void kill () {
+        std::cout << "dispatched kill command from thread " << std::this_thread::get_id() << '\n';
         m_isRunning = false;
         if (m_isPaused)
             m_cv.notify_one();
     }
     void call (message_type fcn) {
+        std::cout << "dispatched call from thread " << std::this_thread::get_id() << '\n';
         m_queue.push(new message_type(fcn));
         if (m_isPaused)
             m_cv.notify_one();
@@ -151,13 +164,24 @@ public:
     JSThreadedInstance () :
         m_thread(threadTask())
     {}
+    ~JSThreadedInstance () {
+        kill();
+        m_thread.join();
+//        m_thread.detach();
+    }
     
     std::function<void(void)> threadTask () {
+        std::cout << "Starting js platform instance from thread " << std::this_thread::get_id() << '\n';
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         return [=] () {
             try {
+                std::cout << "Starting js platform instance on thread " << std::this_thread::get_id() << '\n';
+                
                 JSPlatform platform;
                 m_dispatchQueue.setDispatchTarget(&platform);
                 m_dispatchQueue.run();
+                
+                std::cout << "Js platform thread ending (" << std::this_thread::get_id() << '\n';
             } catch (const std::runtime_error & e) {
                 std::cout << "JS worker thread failed with exception:\n\t" << e.what() << std::endl;
             }
@@ -166,6 +190,7 @@ public:
 #define RUN_ON_WORKER_THREAD m_dispatchQueue.call([=](JSPlatform &platform)
     
     void eval (const std::string & contents) {
+        std::cout << "scheduling eval task\n";
         RUN_ON_WORKER_THREAD {
             platform.eval(contents);
         });
